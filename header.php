825d76e2a4901b4d08e0b86c2e0ee242e2788f91
nicholewagner1 <nicholemwagner@gmail.com>
2024-02-05 16:44:29 -0600

    icons

--- header.php
+++ header.php
@@ -21,7 +21,10 @@ require($_SERVER['DOCUMENT_ROOT'].'/api/auth.php');
 	<link href="/utils/fontawesome-free-6.5/css/fontawesome.css" rel="stylesheet">
 	  <link href="/utils/fontawesome-free-6.5/css/solid.css" rel="stylesheet">
 	<script src="/js/common.js"></script>
-
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/images/favicon/site.webmanifest">
 	<link rel="stylesheet" href="style.css">
 
 </head>
